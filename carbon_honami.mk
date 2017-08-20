2986bc02a91dc43ab4e735ef1b96b4485691663c
Nikhil Punathil <nikhilpe@gmail.com>
2017-08-20 13:14:26 +0530

    honami: Carbonify
    *Remove unsupported overlays too
    
    Change-Id: I4724440b6f290bcdea34e2cbf7680b5c4095fce5
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- carbon_honami.mk
+++ carbon_honami.mk
@@ -1,11 +1,13 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
+# Inherit Carbon GSM telephony parts
+ $(call inherit-product, vendor/carbon/config/gsm.mk)
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.1.1/14.6.A.1.236/2031203603:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6903-user 5.1.1 14.6.A.1.236 2031203603 release-keys"
 
-PRODUCT_NAME := lineage_honami
+PRODUCT_NAME := carbon_honami
 PRODUCT_DEVICE := honami
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_honami-eng
-add_lunch_combo lineage_honami-userdebug
